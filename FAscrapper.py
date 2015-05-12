dca414df1a87e03be23691d84bd8cba33b18d3b2
Elu Risoto <elurisoto@gmail.com>
2015-05-12 14:02:31 +0200

    Functions

--- FAscrapper.py
+++ FAscrapper.py
@@ -1,17 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
+def getSearchURL(title,year):
+	return "http://www.filmaffinity.com/en/advsearch.php?stext=" + title + "&fromyear=" + str(year) + "&toyear=" +str(year)
 
-# Generar url...
 
-session = requests.Session()
-url = "http://www.filmaffinity.com/en/film861601.html"
-r = session.get(url)
-r.encoding='UTF-8'
+def getMovieURL(searchURL):
+	session = requests.Session()
+	r = session.get(searchURL)
+	r.encoding='UTF-8'
 
-data = r.text
-soup = BeautifulSoup(data)
+	data = r.text
+	soup = BeautifulSoup(data)
 
-nota = soup.find(id="movie-rat-avg").get_text()
-nota = float(nota.strip())
-print nota
+	moviedata = soup.find("div",{"class":"mc-title"}).find('a')
+	return "http://www.filmaffinity.com" + moviedata['href']
+
+
+def getRating(movieURL):
+	session = requests.Session()
+	r = session.get(movieURL)
+	r.encoding='UTF-8'
+
+	data = r.text
+	soup = BeautifulSoup(data)
+
+	nota = soup.find(id="movie-rat-avg").get_text()
+	return float(nota.strip())
+
+def FARating(title,year):
+	searchurl = getSearchURL(title, year)
+	movieurl = getMovieURL(searchurl)
+	return getRating(movieurl)
+
