d0b57f76ed95bc5a586c0d4d226e841893fc02ad
Elu Risoto <elurisoto@gmail.com>
2015-05-13 18:19:22 +0200

    Final function

--- downloader.py
+++ downloader.py
@@ -23,9 +23,13 @@ def download(titles):
 	for i,movie in enumerate(titles):
 		response = omdb.title(movie[0],year=movie[1], tomatoes=True)
 		if response:
-			FA = FARating(movie[0],movie[1])
-			response['FA_rating']=str(FA)
-			data.append(response) # For some movies with numbers in the title it doesnt answer, don't know why
+			if response['tomato_rating'] != 'N/A':
+				# print response['tomato_rating']
+				# if response['tomato_rating'] == 'N/A':
+				# 	print response['title']
+				FA = FARating(movie[0],movie[1])
+				response['FA_rating']=str(FA)
+				data.append(response) # For some movies with numbers in the title it doesnt answer, don't know why
 		progress = (i+1.0)/total
 		sys.stdout.write("\r[" + "=" * (int(round((progress*size)))-1) +">" +  " " * int(round((1-progress)*size)) + "] "+ str(i+1) + "/" + str(total) + " (" + str(progress*100) + "%)")
 		sys.stdout.flush()
@@ -38,7 +42,9 @@ def writeCSV(data, filename):
 
 	with open('data/' + filename, 'w') as csvfile:
 		
-		fieldsToSave = ['title','year','metascore','FA_rating','tomato_rating','tomato_meter','tomato_rotten','tomato_user_meter','tomato_fresh','imdb_votes','tomato_user_reviews','imdb_id','tomato_reviews','awards','tomato_user_rating','genre','imdb_rating','country','runtime','box_office']
+		fieldsToSave = ['title','year','metascore','FA_rating','tomato_rating','tomato_meter','tomato_rotten',
+						'tomato_user_meter','tomato_fresh','imdb_votes','tomato_user_reviews','imdb_id','tomato_reviews',
+						'awards','tomato_user_rating','genre','imdb_rating','country','runtime','box_office']
 
 		writer = csv.DictWriter(csvfile, fieldnames=fieldsToSave)
 
@@ -48,6 +54,7 @@ def writeCSV(data, filename):
 			writer.writerow({key:movie[key].encode('utf8') for key in fieldsToSave})
 
 
-titles = pullFromCSV("data/lbxd/ratings.csv")
-data=download(titles)
-writeCSV(data,"output.csv")
+def pullAndSave(movieListPath, outputFile):
+	titles = pullFromCSV(movieListPath)
+	data=download(titles)
+	writeCSV(data,outputFile)
