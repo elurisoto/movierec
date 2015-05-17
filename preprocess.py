a4d080c42a498fceaef85873e94fdf7afeed0096
Elu Risoto <elurisoto@gmail.com>
2015-05-17 18:56:59 +0200

    Output format changes

--- preprocess.py
+++ preprocess.py
@@ -1,17 +1,21 @@
 import csv,copy, re
 
+#############################################################################
+# Here we preprocess the data to pass it to Matlab's neural network toolbox #
+#############################################################################
+
 possibleGenres = {
-'Action':		False,		'Adventure':	False,
-'Animation':	False,		'Biography':	False,
-'Comedy':		False,		'Crime':		False,
-'Documentary':	False,		'Drama':		False,
-'Family':		False,		'Fantasy':		False,
-'Film-Noir':	False,		'History':		False,
-'Horror':		False,		'Music':		False,
-'Musical':		False,		'Mystery':		False,
-'Romance':		False,		'Sci-Fi':		False,
-'Sport':		False,		'Thriller':		False,
-'War':			False,		'Western':		False
+'Action':		0,		'Adventure':	0,
+'Animation':	0,		'Biography':	0,
+'Comedy':		0,		'Crime':		0,
+'Documentary':	0,		'Drama':		0,
+'Family':		0,		'Fantasy':		0,
+'Film-Noir':	0,		'History':		0,
+'Horror':		0,		'Music':		0,
+'Musical':		0,		'Mystery':		0,
+'Romance':		0,		'Sci-Fi':		0,
+'Sport':		0,		'Thriller':		0,
+'War':			0,		'Western':		0
 }
 
 oscarsRE = re.compile("Won (?:\d*\.)?\d+ Oscar")
@@ -28,7 +32,7 @@ def convertGenres(genres):
 	genres = genres.replace(' ','').split(',')
 	dictionary = copy.copy(possibleGenres)
 	for genre in genres:
-		dictionary[genre] = True
+		dictionary[genre] = 1
 	return dictionary
 
 # Returns the number of oscars that a movie recieved
@@ -39,16 +43,32 @@ def processOscars(text):
 		numOscars = (numberRE.search(oscars.group(0))).group(0)
 	return numOscars
 
+def processBoxOffice(text):
+	if text[-1]=='M':
+		return float(text.replace('$','').replace('M',''))
+	elif text[-1]=='k':
+		k = float(text.replace('$','').replace('k',''))
+		return k/1000.0
+	else:
+		return text
 
-# Changes the format of some rows to make the data easier to compute
+# Changes the format of some rows to make the data easier to compute 
+# and delete some string rows since matlab doesn't like them.
 def adaptFormat(data):
 	for row in data:
 		row['imdb_votes'] = row['imdb_votes'].replace(',','')
-		row['box_office'] = row['box_office'].replace('$','').replace('M','')
+		
 		row['runtime'] = row['runtime'].replace(' min','')
+		row['imdb_id'] = row['imdb_id'].replace('tt','')
 		genres = convertGenres(row['genre'])
 		row.update(genres)
 		row['oscars'] = processOscars(row['awards'])
+		row['box_office'] = processBoxOffice(row['box_office'])
+
+		del row['awards']
+		del row['genre']
+		del row['title']
+		del row['country']
 
 	return data
 
@@ -61,8 +81,7 @@ def save(data, filename):
 			writer.writerow({key:movie[key] for key in movie.keys()})
 
 if __name__=="__main__":
-	data = loadCSV("data/output.csv")
+	data = loadCSV("data/outputAlex.csv")
 	data = adaptFormat(data)
-	save(data, "data/outputPreprocessed.csv")
-
+	save(data, "data/outputAlexPreprocessed.csv")
 
