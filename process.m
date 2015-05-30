5ae1c7e7b5a91e40b51085315b4d9b87a126d1b1
Elu Risoto <elurisoto@gmail.com>
2015-05-30 19:14:57 +0200

    Neural network and MSE

--- process.m
+++ process.m
@@ -1,2 +1,13 @@
 format bank;
-data = importfile('data/outputAlexPreprocessed.csv');
+data = importData('data/outputAlexPreprocessed.csv');
+target = data(:,30)
+
+%target = importTarget('data/outputAlexPreprocessed.csv');
+
+%Extraemos las variables que vamos a usar por ahora, la puntuación de imdb,
+%la de filmaffinity y la de los usuarios de rotten tomatoes
+data = data(:,[1,18,27]);
+
+data = data';
+results = myNeuralNetworkFunction(data);
+MSE = sum((results - target').^2)/length(results);
