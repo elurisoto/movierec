1e48ab5e9953965df6cf0b594da6cfdfe155baaf
Elu Risoto <elurisoto@gmail.com>
2015-06-27 19:06:40 +0200

    Graph pca

--- pca.R
+++ pca.R
@@ -6,8 +6,22 @@ types <- lapply(data,class)
 numerics <- data[types=="integer" | types =="numeric"][-17]
 target <- data$user_rating
 
-#Keep only variables that may have useful information
-
 # Missing value imputation
 imputed <- impKNNa(numerics, primitive=TRUE, metric = "Euclidean", k=17)$xImp
 
+numerics.pca <- prcomp(imputed, center=TRUE, scale=TRUE)
+summary(numerics.pca)
+
+
+
+library(devtools)
+install_github("ggbiplot", "vqv")
+
+library(ggbiplot)
+g <- ggbiplot(numerics.pca, obs.scale = 1, var.scale = 1, 
+							 ellipse = TRUE, 
+							circle = TRUE)
+g <- g + scale_color_discrete(name = '')
+g <- g + theme(legend.direction = 'horizontal', 
+							 legend.position = 'top')
+print(g)
